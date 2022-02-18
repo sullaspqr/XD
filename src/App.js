aabfba7da00d9698e1b104c146d058c8b5cc93db
sullaspqr <sulla.spqr78@gmail.com>
2022-02-18 08:41:47 +0100

    tesztelés

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Tanuljunk Reactot!
         </a>
       </header>
     </div>
